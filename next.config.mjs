598dd2f200f7190cb75640aaf53f5b95440db61c
Hamza Yaghmmour <hamza@moneyflap.com>
2024-10-06 11:48:01 -0400

    next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,    // Enables strict mode
   swcMinify: true,          // Minify with SWC for faster builds
-  output: 'export',
-  basePath: '/portfolio',
-  assetPrefix: '/portfolio',
-
 };
 
 export default nextConfig;
