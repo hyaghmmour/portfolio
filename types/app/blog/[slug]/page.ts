b32673fd1997d51e36f245f249de7a95fada5153
hyaghmmour <hyaghmmour@users.noreply.github.com>
2024-10-06 06:05:54 +0000

    Build: (55d8ce1) moved app folder

--- types/app/blog/[slug]/page.ts
+++ types/app/blog/[slug]/page.ts
@@ -1,8 +1,8 @@
-// File: /home/runner/work/portfolio/portfolio/src/app/blog/[slug]/page.tsx
-import * as entry from '../../../../../src/app/blog/[slug]/page.js'
+// File: /home/runner/work/portfolio/portfolio/app/blog/[slug]/page.tsx
+import * as entry from '../../../../../app/blog/[slug]/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../../../src/app/blog/[slug]/page.js')
+type TEntry = typeof import('../../../../../app/blog/[slug]/page.js')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
