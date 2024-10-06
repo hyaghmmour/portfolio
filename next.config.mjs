15e4716ccd6626f6bb59b24faa0e2f435d3f1d4c
Hamza Yaghmmour <hamza@moneyflap.com>
2024-10-06 01:17:53 -0400

    rerouting

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true,    // Enables strict mode
+  swcMinify: true,          // Minify with SWC for faster builds
 };
 
 export default nextConfig;
